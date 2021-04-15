29a0189ed8265462bbd9f829949a229a17080fff
Nasir-Jamal <39132449+Nasir-Jamal@users.noreply.github.com>
2021-04-16 01:06:52 +0800

    Updated Room.js states

--- src/Room.js
+++ src/Room.js
@@ -2,11 +2,30 @@ import React, {useState} from 'react';
 
 function Room()
 {
-    const [isLit, setLit] = useState(false);
+    let [isLit, setLit] = useState(false);
+    let [age, setAge] = useState(15);
+
+    function updateLit()
+    {
+        console.log("Button Clicked");
+        setLit(!isLit);
+    }
+
+    function inceaseAge()
+    {
+        console.log("Increas Age Button Clicked");
+        setAge(++age);
+    }
 
     return(
         <div>
             This Room is Lit = {isLit ? "lit" : "dark"}
+            <br/>
+            Age: {age};
+            <br/>
+            <button onClick={updateLit}>Toggle Light</button>
+            <br/>
+            <button onClick={inceaseAge}>Increase Age</button>
         </div>
     );
 }
