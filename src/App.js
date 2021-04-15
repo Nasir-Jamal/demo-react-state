68d459de9728f1bf6853b1a343ae97693c369240
Nasir-Jamal <39132449+Nasir-Jamal@users.noreply.github.com>
2021-04-16 01:32:23 +0800

    update basic react state demo

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Room from './Room.js';
+import Room_Function_Restructuring from './Room_Function_Restructuring';
 
 function App() {
   return (
    <div>
      <Room></Room>
+     <br/>
+     <br/>
+     <h1>From Room_Function_Restructuring.js</h1>
+     <Room_Function_Restructuring></Room_Function_Restructuring>
    </div>
+    
   );
 }
 
---
+++ src/Room_Function_Restructuring.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+
+function Room_Function_Restructuring()
+{
+let [isLit, setLit] = useState(false); //Array restructuring
+let [age, setAge] = useState(15); //Array restrucuting
+
+//function restrcuting: Arrow function example
+/* function updateLit(){
+    console.log("Button Clicked");
+    setLit(!isLit);
+}*/
+//should be as in restructuring form
+const updateLit = () => {
+    console.log("Button Clicked");
+    setLit(!isLit);
+}
+
+const increaseAge = () =>{
+    console.log("Increase Age Button Clicked");
+    setAge(++age);
+}
+
+return(
+    <div>
+        The Room is {isLit ? "Lit" : "dark"},
+        <br/>
+        Age: {age},
+        <br/>
+        <button onClick={updateLit}>Update Light</button>
+        <br/> 
+        <button onClick={increaseAge}>Age Counter</button>
+    </div>
+
+);
+/* function call onClick inside button
+<button onClick={()=>{
+                    console.log("Increase Age Button Clicked");
+                    setAge(++age);
+                     }}>
+    Increase Age
+    </button>
+    */
+//<button onClick={()=>setLit(!isLit);}     
+}
+export default Room_Function_Restructuring;
